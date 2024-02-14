a86ec635a4edd8b8b87ecc5d0e2263959d105ba0
shanbrick <sbrickett27@gmail.com>
2024-02-14 17:12:23 -0500

    initial commit

---
+++ site.js
@@ -0,0 +1,20 @@
+function newDiv() {
+    const para = document.createElement("p");
+    para.innerText = "This is a paragraph";
+    document.body.appendChild(para);
+}
+
+function addToUnsorted(imgLink, stageName, groupName) {
+
+}
+
+const unsortedList = [
+    {
+        "groupName" : "Xdinary Heroes",
+        "#" : 1,
+        "stageName" : "O.de",
+        "fullName" : "Oh Seungmin",
+        "imgLink" : "ode.jpeg",
+        "birthday" : "June 11, 2002"
+    }
+]
