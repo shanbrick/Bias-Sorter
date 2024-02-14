ff933b71f9d464ba5c1d155512d2762b979dae78
shanbrick <sbrickett27@gmail.com>
2024-02-14 18:15:49 -0500

    working on code to add new div using javascript

--- site.js
+++ site.js
@@ -4,17 +4,42 @@ function newDiv() {
     document.body.appendChild(para);
 }
 
-function addToUnsorted(imgLink, stageName, groupName) {
-
+function newUnsortedDiv() {
+    const div = document.createElement("div");
 }
 
-const unsortedList = [
-    {
-        "groupName" : "Xdinary Heroes",
-        "#" : 1,
-        "stageName" : "O.de",
-        "fullName" : "Oh Seungmin",
-        "imgLink" : "ode.jpeg",
-        "birthday" : "June 11, 2002"
-    }
-]
+// <div class="groupPageDivs">
+//      <img class="personPicIndividual" src="ode.jpeg"></img>
+//      <p id="stageName1" class="idolNameGroupPage">O.de</p>
+//      <button onclick="addToUnsorted('BOYNEXTDOOR', 3, 'Myung Jaehyun', 'Myung Jaehyun', 'December 4, 2003', 'myungJaehyun.jpeg')">Add</button>
+// </div>
+
+function addToUnsorted(groupName, id, stageName, fullName, birthday, imgLink) {
+    unsortedList.push(
+        {
+            "groupName" : groupName,
+            "#" : id,
+            "stageName" : stageName,
+            "fullName" : fullName,
+            "birthday" : birthday,
+            "imgLink" : imgLink
+        }
+    )
+
+    console.log(unsortedList);
+}
+
+// const unsortedList = [
+//     {
+//         "groupName" : "Xdinary Heroes",
+//         "#" : 4,
+//         "stageName" : "O.de",
+//         "fullName" : "Oh Seungmin",
+//         "birthday" : "June 11, 2002",
+//         "imgLink" : "ode.jpeg"
+//     }
+// ]
+
+// console.log(unsortedList)
+// addToUnsorted("BOYNEXTDOOR", 3, "Myung Jaehyun", "Myung Jaehyun", "December 4, 2003", "myungJaehyun.jpeg")
+// console.log(unsortedList)
