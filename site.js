154da7d0c413a7bb02dba84f9af3d5c70588dcc5
shanbrick <sbrickett27@gmail.com>
2024-02-14 23:03:05 -0500

    big json, homepage divs

--- site.js
+++ site.js
@@ -1,11 +1,31 @@
-function newDiv() {
-    const para = document.createElement("p");
-    para.innerText = "This is a paragraph";
-    document.body.appendChild(para);
-}
+fetch('Groups.json')
+  .then(response => response.json())
+  .then(archive => console.log(archive))
+.catch(error => console.error('Error fetching JSON:', error));
 
-function newUnsortedDiv() {
-    const div = document.createElement("div");
+function homePageDiv(list, category) {
+    list.forEach(person => {
+
+        const div = document.createElement('div');
+        div.setAttribute('class','peopleUnsorted');
+    
+        const picture = document.createElement('img');
+        picture.setAttribute('class','homePage');
+        picture.src = person.imgLink;
+        div.appendChild(picture);
+    
+        const name = document.createElement('p');
+        name.setAttribute('class','idolName');
+        name.textContent = person.stageName;
+        div.appendChild(name);
+    
+        const group = document.createElement('p');
+        group.setAttribute('class','groupName');
+        group.textContent = person.groupName;
+        div.appendChild(group);
+
+        document.getElementById(category).appendChild(div);
+    })
 }
 
 // <div class="groupPageDivs">
@@ -17,29 +37,74 @@ function newUnsortedDiv() {
 function addToUnsorted(groupName, id, stageName, fullName, birthday, imgLink) {
     unsortedList.push(
         {
-            "groupName" : groupName,
-            "#" : id,
-            "stageName" : stageName,
-            "fullName" : fullName,
-            "birthday" : birthday,
-            "imgLink" : imgLink
+            "groupName": groupName,
+            "#": id,
+            "stageName": stageName,
+            "fullName": fullName,
+            "birthday": birthday,
+            "imgLink": imgLink
         }
     )
 
     console.log(unsortedList);
 }
 
-// const unsortedList = [
-//     {
-//         "groupName" : "Xdinary Heroes",
-//         "#" : 4,
-//         "stageName" : "O.de",
-//         "fullName" : "Oh Seungmin",
-//         "birthday" : "June 11, 2002",
-//         "imgLink" : "ode.jpeg"
-//     }
-// ]
+const unsortedList = [
+    {
+        "groupName": "Xdinary Heroes",
+        "#": 4,
+        "stageName": "O.de",
+        "fullName": "Oh Seungmin",
+        "birthday": "June 11, 2002",
+        "imgLink": "ode.jpeg"
+    },
+    {
+        "groupName": "BOYNEXTDOOR",
+        "#": 3,
+        "stageName": "Myung Jaehyun",
+        "fullName": "Myung Jaehyun",
+        "birthday": "December 4, 2003",
+        "imgLink": "myungJaehyun.jpeg"
+    },
+    {
+        "groupName": "RIIZE",
+        "#": 4,
+        "stageName": "Wonbin",
+        "fullName": "Park Wonbin",
+        "birthday": "March 2, 2002",
+        "imgLink": "wonbin.jpeg"
+    }
+]
 
-// console.log(unsortedList)
-// addToUnsorted("BOYNEXTDOOR", 3, "Myung Jaehyun", "Myung Jaehyun", "December 4, 2003", "myungJaehyun.jpeg")
-// console.log(unsortedList)
+const ultList = [
+    {
+        "groupName": "Xdinary Heroes",
+        "#": 4,
+        "stageName": "O.de",
+        "fullName": "Oh Seungmin",
+        "birthday": "June 11, 2002",
+        "imgLink": "ode.jpeg"
+    }
+]
+
+const semiList = [
+    {
+        "groupName": "BOYNEXTDOOR",
+        "#": 3,
+        "stageName": "Myung Jaehyun",
+        "fullName": "Myung Jaehyun",
+        "birthday": "December 4, 2003",
+        "imgLink": "myungJaehyun.jpeg"
+    }
+]
+
+const regList = [
+    {
+        "groupName": "RIIZE",
+        "#": 4,
+        "stageName": "Wonbin",
+        "fullName": "Park Wonbin",
+        "birthday": "March 2, 2002",
+        "imgLink": "wonbin.jpeg"
+    }
+]
