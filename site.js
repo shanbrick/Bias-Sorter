e7b16886f6b76af7c67fa3a2c579e341676fe977
shanbrick <sbrickett27@gmail.com>
2024-02-17 17:53:40 -0500

    trying local storage

--- site.js
+++ site.js
@@ -28,27 +28,6 @@ function homePageDiv(list, category) {
     })
 }
 
-// <div class="groupPageDivs">
-//      <img class="personPicIndividual" src="ode.jpeg"></img>
-//      <p id="stageName1" class="idolNameGroupPage">O.de</p>
-//      <button onclick="addToUnsorted('BOYNEXTDOOR', 3, 'Myung Jaehyun', 'Myung Jaehyun', 'December 4, 2003', 'myungJaehyun.jpeg')">Add</button>
-// </div>
-
-function addToUnsorted(groupName, id, stageName, fullName, birthday, imgLink) {
-    unsortedList.push(
-        {
-            "groupName": groupName,
-            "#": id,
-            "stageName": stageName,
-            "fullName": fullName,
-            "birthday": birthday,
-            "imgLink": imgLink
-        }
-    )
-
-    console.log(unsortedList);
-}
-
 const unsortedList = [
     {
         "groupName": "Xdinary Heroes",
@@ -75,6 +54,55 @@ const unsortedList = [
         "imgLink": "wonbin.jpeg"
     }
 ]
+
+function homePageDiv2(category) {
+    localStorage.setItem('unsorted',JSON.stringify(unsortedList));
+    let unsortedArray = JSON.parse(localStorage.getItem('unsorted'));
+
+    unsortedArray.forEach(person => {
+
+        const div = document.createElement('div');
+        div.setAttribute('class','peopleUnsorted');
+    
+        const picture = document.createElement('img');
+        picture.setAttribute('class','homePage');
+        picture.src = person.imgLink;
+        div.appendChild(picture);
+    
+        const name = document.createElement('p');
+        name.setAttribute('class','idolName');
+        name.textContent = person.stageName;
+        div.appendChild(name);
+    
+        const group = document.createElement('p');
+        group.setAttribute('class','groupName');
+        group.textContent = person.groupName;
+        div.appendChild(group);
+
+        document.getElementById(category).appendChild(div);
+    })
+}
+
+// <div class="groupPageDivs">
+//      <img class="personPicIndividual" src="ode.jpeg"></img>
+//      <p id="stageName1" class="idolNameGroupPage">O.de</p>
+//      <button onclick="addToUnsorted('BOYNEXTDOOR', 3, 'Myung Jaehyun', 'Myung Jaehyun', 'December 4, 2003', 'myungJaehyun.jpeg')">Add</button>
+// </div>
+
+function addToUnsorted(groupName, id, stageName, fullName, birthday, imgLink) {
+    unsortedList.push(
+        {
+            "groupName": groupName,
+            "#": id,
+            "stageName": stageName,
+            "fullName": fullName,
+            "birthday": birthday,
+            "imgLink": imgLink
+        }
+    )
+
+    console.log(unsortedList);
+}
 
 const ultList = [
     {
