61b416479ea0c7fe048ff26d9dff3fe781fbcf3d
shanbrick <sbrickett27@gmail.com>
2024-02-27 00:33:13 -0500

    starting to convert to vue

--- bias-sorter/src/App.vue
+++ bias-sorter/src/App.vue
@@ -1,30 +1,94 @@
 <template>
+  <div class="header">
+    <h1>Bias Sorter</h1>
+  </div>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
+    <router-link to="/boy-groups">Boy Groups</router-link>
+    <router-link to="/girl-groups">Girl Groups</router-link>
+    <router-link to="/solo">Solo</router-link>
+    <input class="right" type="text" placeholder="Search...">
   </nav>
-  <router-view/>
+  <router-view />
 </template>
+
+<script>
+import groupList from './groupList.json'
+
+export default {
+  data() {
+    return { groupData: groupList }
+  }
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Trebuchet MS;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
+  background: #ededed;
+}
+
+* {
+  box-sizing: border-box;
 }
 
 nav {
-  padding: 30px;
+  overflow: hidden;
+  background-color: #b3b8e9;
+  font-weight: bold;
+  font-size: 16px;
 }
 
 nav a {
+  float: left;
+  display: block;
+  color: white;
   font-weight: bold;
-  color: #2c3e50;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  text-align: center;
+  text-decoration: none;
+  font-weight: 700;
+}
+
+.nav a:hover {
+  background-color: #8d91b7;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: white;
+}
+
+input {
+  float: right;
+  margin-right: 10px;
+  margin-top: 6px;
+  font-weight: 500;
+  font-size: 15px;
+  font-family: Trebuchet MS;
+  color: white;
+  border-radius: 5px;
+  border-color: #50a0d900;
+  background-color: #8d91b7;
+  padding: 7px;
+}
+
+.header {
+  padding: 5px;
+  text-align: center;
+  background: #b3b8e9;
+  color: white;
+}
+
+.header h1 {
+  margin-left: 10px;
+  font-size: 35px;
+  line-height: 20px;
+  font-weight: 800;
 }
 </style>
--- bias-sorter/src/views/HomeView.vue
+++ bias-sorter/src/views/HomeView.vue
@@ -1,15 +1,38 @@
-<template>
-  <div class="home">
-    <p>Name: {{ name }}</p>
-    <button @click="changeName">Change name</button>
-    <p v-if="name === 'Alex'">Cool name!!!</p>
-    <div v-else>
-      <img alt="Vue logo" src="../assets/logo.png">
-      <HelloWorld msg="Welcome to Your Vue.js App"/>
-    </div>
+<!-- <p v-if="name === 'Alex'">Cool name!!!</p>
+<div v-else>
+  <img alt="Vue logo" src="../assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</div>
+<div v-for="person in people">
+  <p>{{ person.name }}</p>
+</div> 
+<p>Name: {{ name }}</p>
+<button @click="changeName">Change name</button>
+-->
 
-    <div v-for="person in people">
-      <p>{{ person.name }}</p>
+<template>
+  <div id="unsorted" class="unsorted">
+    <div class="boxHeaderUnsorted">
+      <p class="categoryTitle">Unsorted</p>
+      <p class="idolName" v-for="person in groupData">{{ person.groupName }}</p>
+    </div>
+  </div>
+
+  <div id="ult" class="categories">
+    <div class="boxHeaderCategories">
+      <p class="categoryTitle">Ults</p>
+    </div>
+  </div>
+
+  <div id="semi" class="categories">
+    <div class="boxHeaderCategories">
+      <p class="categoryTitle">Semis</p>
+    </div>
+  </div>
+
+  <div id="reg" class="categories">
+    <div class="boxHeaderCategories">
+      <p class="categoryTitle">Regs</p>
     </div>
   </div>
 </template>
@@ -17,6 +40,7 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import groupList from '../groupList.json'
 
 export default {
   name: 'HomeView',
@@ -27,15 +51,11 @@ export default {
 
   ],
   mounted() {
-    console.log("hi shannon!!!!!! isn't vue fun?")
+
   },
-  data: () => {
+  data() {
     return {
-      name: 'Shannon',
-      people: [
-        { name: 'Juliet'},
-        { name: 'Shan'}
-      ]
+      groupData: groupList
     }
   },
   computed: {
@@ -45,12 +65,99 @@ export default {
     changeName() {
       this.name = 'Alex'
 
-      this.people = [ ...this.people, { name: 'Alex' }]
+      this.people = [...this.people, { name: 'Alex' }]
     }
   }
 }
 </script>
 
 <style scoped>
+.unsorted {
+  height: 60%;
+  width: 342px;
+  position: fixed;
+  z-index: 1;
+  overflow-x: hidden;
+  /* disable horizontal scroll */
+  background-color: #ffffff;
+  border-bottom: 1px solid #B0B0B0;
+  border-left: 1px solid #B0B0B0;
+  border-right: 1px solid #B0B0B0;
+  padding: 10px;
+  padding-left: 0px;
+  padding-right: 0px;
+  margin: 20px;
+  margin-top: 65px;
+  border-radius: 0px 0px 10px 10px;
+}
 
+.boxHeaderUnsorted {
+  height: 45px;
+  width: 342px;
+  background-color: #b3b8e9;
+  border-top: 1px solid #B0B0B0;
+  border-left: 1px solid #B0B0B0;
+  border-right: 1px solid #B0B0B0;
+  border-radius: 10px 10px 0px 0px;
+  padding-left: 20px;
+  margin-top: -55px;
+  margin-left: -1px;
+  margin-bottom: 15px;
+  position: fixed;
+}
+
+.categories {
+  margin: 20px;
+  margin-left: 387px;
+  margin-top: 65px;
+  position: relative;
+  background-color: #ffffff;
+  padding: 10px;
+  padding-left: 0px;
+  padding-right: 0px;
+  border-radius: 0px 0px 10px 10px;
+  border-bottom: 1px solid #B0B0B0;
+  border-left: 1px solid #B0B0B0;
+  border-right: 1px solid #B0B0B0;
+}
+
+.categoryTitle {
+    top: -8px;
+    position: absolute;
+    z-index: 1;
+    color: white;
+    font-size: 20px;
+    font-weight: 700;
+}
+
+.categories:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.boxHeaderCategories {
+  height: 45px;
+  background-color: #b3b8e9;
+  border-top: 1px solid B0B0B0;
+  border-left: 1px solid #B0B0B0;
+  border-right: 1px solid #B0B0B0;
+  border-radius: 10px 10px 0px 0px;
+  padding: 20px;
+  margin-top: -55px;
+  /*padding categories + boxheader height*/
+  margin-bottom: 10px;
+  margin-left: -1px;
+  /* categories padding*/
+  margin-right: -1px;
+  position: relative;
+}
+
+.idolName {
+  text-align: center;
+  font-size: 15px;
+  margin: 5px;
+  font-weight: bold;
+  color: black;
+}
 </style>
